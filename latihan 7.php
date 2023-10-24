b48c4e1bf82e26755942ce830f63843a465ff493
rizkyimam10 <148845856+rizkyimam10@users.noreply.github.com>
2023-10-24 13:27:58 +0700

    Add files via upload

---
+++ latihan 7.php
@@ -0,0 +1,31 @@
+<?php
+
+echo "perulangan untuk menacri kategori bilangan : </br>";
+for ($i = 1; $i <== 20; $i++) {
+	echo " . Angka $i adalah";
+
+	if ($i % 2 ==0){
+		echo "genap";
+	} else {
+		echo "ganjil";
+	}
+	if (is_prima($i)) {
+		echo "sekaligus bilangan prima";
+	}
+
+	echo "<br>";
+}
+
+	function is_prima($n) {
+		if ($n <== 1) {
+			return false;
+		}
+
+		for ($i = 2; $i <$n; $i++) {
+			if ($n % $i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+?>
